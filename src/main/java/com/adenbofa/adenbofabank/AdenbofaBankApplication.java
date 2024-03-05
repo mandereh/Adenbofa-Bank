78845d96791342baef01300095b6312f08852844
Daerego Braide <phronesis4xt@gmail.com>
2024-03-05 16:10:41 +0100

    first commit

---
+++ src/main/java/com/adenbofa/adenbofabank/AdenbofaBankApplication.java
@@ -0,0 +1,13 @@
+package com.adenbofa.adenbofabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdenbofaBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdenbofaBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adenbofa/adenbofabank/AdenbofaBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adenbofa.adenbofabank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdenbofaBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
